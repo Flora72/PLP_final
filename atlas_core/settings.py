38744f9c2f5eebd74b3e5e55943c0aeb1a3199e1
Flora72 <zahratheo569@gmail.com>
2025-11-09 14:48:01 +0300

    Fix typo in ALLOWED_HOSTS for correct domain name

--- atlas_core/settings.py
+++ atlas_core/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security
 SECRET_KEY = os.getenv("SECRET_KEY")  
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'healingatlas.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'healing_atlas.onrender.com']
 
 
 # Installed apps
